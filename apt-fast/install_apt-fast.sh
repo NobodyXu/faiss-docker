651dd6c10d6735d30f87f38197aa964cadcf807c
nobodyxu <Jiahao_XU@outlook.com>
2020-02-15 11:27:14 +1100

    Add apt-fast/ into /tmp/apt-fast/
    so that it will not be mixed with other file in /tmp

--- apt-fast/install_apt-fast.sh
+++ apt-fast/install_apt-fast.sh
@@ -4,6 +4,7 @@
 apt-get update
 apt-get install -y aria2 coreutils wget sed
 
+cd /tmp
 wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
 chmod +x /usr/local/sbin/apt-fast
 
@@ -13,4 +14,4 @@ if [ $core_cnt -lt 5 ]; then
     core_cnt=5
 fi
 
-exec sed "s/core_cnts/$(nproc)/" /tmp/apt-fast.conf | tee /etc/apt-fast.conf
+exec sed "s/core_cnts/$(nproc)/" /tmp/apt-fast/apt-fast.conf | tee /etc/apt-fast.conf
